f40b0ccf58349058e7920adc75a1a9dd50f05f7d
LABB <unt@labb.email>
2019-03-14 08:31:10 -0400

    Update install and update scripts

--- ula-install.sh
+++ ula-install.sh
@@ -13,12 +13,24 @@ script[2]="apt-upgrade-apps"
 script[3]="dnf-upgrade-apps"
 script[4]="ula-uninstall"
 
-if [ -x "$installPath/${script[0]}" -o -x "$installPath/${script[1]}" -o -x "$installPath/${script[2]}" -o -x "$installPath/${script[3]}" -o -x "$installPath/${script[4]}" ]; then
-    echo "Updating ula scripts on $installPath folder..."
-else
-    echo "Installing ula scripts on $installPath folder..."
-fi
+updatingOrInstalling() {
 
-for i in ${script[@]}; do install "$i.sh" "$installPath/$i"; done
+    if [ -x "$2/$3" ]; then
+        report="Updating"
+    else
+        report="Installing"
+    fi
 
-echo "upgrade-linux-apps installed."
+}
+
+for i in ${script[@]}
+do 
+
+    report="Installing/Updating"
+    updatingOrInstalling $report $installPath $i
+    echo "$report $i..."
+    install "./$i.sh" "$installPath/$i"
+
+done
+
+echo "All ula scripts installed and updated."
--- ula-update-from-git.sh
+++ ula-update-from-git.sh
@@ -16,6 +16,35 @@ git clone https://github.com/abbluiz/upgrade-linux-apps.git ~/.ula-tmp
 
 # TODO: diff
 
-~/.ula-tmp/ula-install.sh
+installPath="/usr/local/bin"
 
+script[0]="ula-update-from-git"
+script[1]="upgrade-linux-apps"
+script[2]="apt-upgrade-apps"
+script[3]="dnf-upgrade-apps"
+script[4]="ula-uninstall"
+
+updatingOrInstalling() {
+
+    if [ -x "$2/$3" ]; then
+        report="Updating"
+    else
+        report="Installing"
+    fi
+
+}
+
+for i in ${script[@]}
+do 
+
+    report="Installing/Updating"
+    updatingOrInstalling $report $installPath $i
+    echo "$report $i..."
+    install "$HOME/.ula-tmp/$i.sh" "$installPath/$i"
+
+done
+
+echo "Deleting temporary directory $HOME/.ula-tmp"
 rm -rf ~/.ula-tmp
+
+echo "All scripts updated from git."
